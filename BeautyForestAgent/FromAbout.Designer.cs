cfcb14b3f93a96630d2db3aea94e14758e787ec6
Jiwon Yoon <s2017i33@e-mirim.hs.kr>
2019-06-24 14:34:58 +0900

    #1 Modal & Modaless 창 띄우기 by 3111 윤지원

--- BeautyForestAgent/FromAbout.Designer.cs
+++ BeautyForestAgent/FromAbout.Designer.cs
@@ -47,7 +47,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(118, 354);
+            this.label1.Location = new System.Drawing.Point(111, 369);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(69, 12);
             this.label1.TabIndex = 1;
